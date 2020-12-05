23cde44425b0c00f069a2965b54aa51e7aa65458
Edis Skenderi <eskenderi@egamingc.com>
2020-12-05 20:14:45 +0100

    finishing

---
+++ assets/js/hover.js
@@ -0,0 +1,52 @@
+/*about */
+
+
+$(".hover-proclub").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+
+$(".hover-armont").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-tapeandgo").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-lightologylab").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-audiovox").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-daam").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-armex").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
+$(".hover-cetka").mouseover(function () {
+    $(this).find('img').attr('src', $(this).find("img").data("hover"));
+}).mouseout(function () {
+    $(this).find("img").attr('src', $(this).find("img").data("src"));
+});
+
